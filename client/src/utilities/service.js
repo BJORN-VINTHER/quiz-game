ec225cead07694751d38489b432a3c9005e299b5
Lasse <lasse.andresen9@gmail.com>
2022-02-13 22:08:57 +0100

    Client test api call

--- client/src/utilities/service.js
+++ client/src/utilities/service.js
@@ -1,18 +1,30 @@
 import { getIp, httpGet, httpPost } from "./utilities";
 import IoClient from "socket.io-client";
 
-export const service = new Service();
-
 class Service {
     constructor() {
     }
 
     async init() {
-        this.ip = getIp()
+        console.log('Initializing service');
+        this.ip = getIp();
+        
     }
 
     connect() {
-        IoClient.connect("...");
+        console.log('Connecting to Socket');
+        const socket = IoClient.connect("https://quiz-game-api.azurewebsites.net");
+        socket.on('connect', () => {
+            console.log('SocketIO connection established');
+            socket.emit('socketAPITest')
+        })
+        return socket;
+    }
+
+    async serverTest() {
+        const result = await httpGet("http://localhost:4000/proofOfConceptGetTest"); // https://quiz-game-api.azurewebsites.net/proofOfConceptGetTest
+        console.log('Server result ', result);
+        return result;
     }
 
     async createGame() {
@@ -34,3 +46,5 @@ class Service {
     }
 
 }
+
+export const service = new Service();
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <script>
 import { serviceMock } from "../utilities/serviceMock.js";
+import { service } from "../utilities/service.js";
 
 export default {
   methods: {
     async onCreateGame() {
       const gameId = await serviceMock.createGame();
       this.$router.push({ path: `/games/${gameId}/lobby` });
+    },
+    async onTestClick() {
+      console.log('Calling service');
+      service.serverTest(); // const socket = service.connect();
     },
   },
 };
@@ -14,6 +19,7 @@ export default {
 <template>
   <div class="d-flex flex-column align-items-center">
     <button @click="onCreateGame">Host new game</button>
+    <button @click="onTestClick">Connection testing</button>
   </div>
 </template>
 
