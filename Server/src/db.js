621e723795084b032082b6283ed45c6b094e5283
Lasse <lasse.andresen9@gmail.com>
2022-02-13 01:07:14 +0100

    Added db calls

--- Server/src/db.js
+++ Server/src/db.js
@@ -4,10 +4,10 @@ var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
 
 export class db {
-    connection;
+    config;
 
     constructor() {
-        var config = {  
+        this.config = {  
             server: 'quiz-game.database.windows.net',
             authentication: {
                 type: 'default',
@@ -22,18 +22,69 @@ export class db {
                 database: 'quiz-game'
             }
         };  
-        this.connection = new Connection(config);
-        this.connection.on('connect', function(err) {  
-            // If no error, then good to proceed.
-            console.log("Connected");
+    }
+
+    async addGame(guid, invitationCode, hostIP) {
+        return new Promise<string>((resolve) => {
+            let gameID;
+
+            const connection = new Connection(config);
+            const request = new Request("INSERT quiz-game.Game (GameGUID, InvitationCode, HostPlayerIP, CreationTime) OUTPUT INSERTED.ID VALUES (@GameGUID, @InvitationCode, @HostIP, CURRENT_TIMESTAMP);", function(err) { 
+                if (err) {  
+                    console.log(err);
+                }  
+            });
+            request.addParameter('GameGUID', TYPES.NVarChar, guid);
+            request.addParameter('InvitationCode', TYPES.NVarChar, invitationCode);
+            request.addParameter('HostIP', TYPES.NVarChar, hostIP);
+            request.on('row', function(columns) {
+                gameID = columns[0].value;
+            });
+    
+            request.on("requestCompleted", function (rowCount, more) {
+                connection.close();
+                resolve(gameID);
+            });
+            connection.execSql(request);
+        });
+    }
+
+    addPlayer(gameID, userName, IP) {
+        return new Promise<string>((resolve) => {
+            let playerID;
+
+            const connection = new Connection(config);
+            const request = new Request("INSERT quiz-game.Player (GameID, UserName, IP) OUTPUT INSERTED.ID VALUES (@GameID, @UserName, @IP);", function(err) { 
+                if (err) {  
+                    console.log(err);
+                }  
+            });
+            request.addParameter('GameID', TYPES.Int, gameID);
+            request.addParameter('UserName', TYPES.NVarChar, userName);
+            request.addParameter('IP', TYPES.NVarChar, IP);
+            request.on('row', function(columns) {
+                playerID = columns[0].value;
+            });
+    
+            request.on("requestCompleted", function (rowCount, more) {
+                connection.close();
+                resolve(playerID);
+            });
+            connection.execSql(request);
         });
     }
 
     executeStatement() {
-        this.connection.connect();
+        const connection = new Connection(config);
+        connection.on('connect', function(err) {  
+            // If no error, then good to proceed.
+            console.log("Connected");
+        });
+         connection.connect();
         const request = new Request("SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;", function(err) {  
-        if (err) {  
-            console.log(err);}  
+            if (err) {  
+                console.log(err);
+            }  
         });  
         var result = "";  
         request.on('row', function(columns) {  
@@ -54,12 +105,17 @@ export class db {
         
         // Close the connection after the final event emitted by the request, after the callback passes
         request.on("requestCompleted", function (rowCount, more) {
-            this.connection.close();
+            connection.close();
         });
-        this.connection.execSql(request);  
+        connection.execSql(request);  
     }
 
     executeStatement1() {  
+        const connection = new Connection(config);
+        connection.on('connect', function(err) {  
+            // If no error, then good to proceed.
+            console.log("Connected");
+        });
         const request = new Request("INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES (@Name, @Number, @Cost, @Price, CURRENT_TIMESTAMP);", function(err) {  
          if (err) {  
             console.log(err);}
@@ -80,8 +136,8 @@ export class db {
 
         // Close the connection after the final event emitted by the request, after the callback passes
         request.on("requestCompleted", function (rowCount, more) {
-            this.connection.close();
+            connection.close();
         });
-        this.connection.execSql(request);  
+        connection.execSql(request);
     }
 }
